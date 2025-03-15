5d7c8d2c84a4f6ac93197cd2f6dd6a647b0b5bae
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-03-15 00:26:23 -0500

    initial commit

---
+++ src/components/ToggleButton.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const ToggleButton = () => {
+    const [isToggled, setIsToggled] = useState(false);
+
+    const handleToggle = () => {
+        setIsToggled(!isToggled);
+    };
+
+    return (
+        <button onClick={handleToggle}>
+            {isToggled ? 'ON' : 'OFF'}
+        </button>
+    );
+};
+
+export default ToggleButton;
